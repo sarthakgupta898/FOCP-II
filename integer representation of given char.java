6be6277444cf529f5742e6c4c16267655c8bfe7f
sarthak gupta <sarthak.gupta129@gmail.com>
2019-01-14 22:05:22 +0530

    ascii

---
+++ integer representation of given char.java
@@ -0,0 +1,8 @@
+class ascii {
+ public static void main(String[] args)
+     {
+        char ch = 'a';
+        int ascii = ch;
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+    }
+}
