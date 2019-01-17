7d57d2de150c0dba5488c488ad18b9e922e61650
sarthak gupta <sarthak.gupta129@gmail.com>
2019-01-17 12:02:03 +0530

    Q1.excercise question data type & control statements

---
+++ basic calculator.java
@@ -0,0 +1,19 @@
+//1.	Write a program that takes two integers (values to be given within the program)
+//      and displays the output of the following operators: addition, subtraction, multiplication, division and modulus
+class first
+{
+	public static void main(String args[])
+	{
+	int a=2,b=3,c;
+	c=a+b;
+	System.out.println("Sum is " + c);
+	c=a-b;
+	System.out.println("Difference is " + c);
+	c=a*b;
+	System.out.println("Product is " + c);
+	c=a/b;
+	System.out.println("Quotient is " + c);
+	c=a%b;
+	System.out.println("Modulus is " + c);
+}
+}
