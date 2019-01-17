c147a8089e310f4869957c9a680fe62c2e6b519c
sarthak gupta <sarthak.gupta129@gmail.com>
2019-01-17 12:07:22 +0530

    Q3.exercise question control statements & data types

---
+++ relational operators.java
@@ -0,0 +1,13 @@
+//3.	Write a program to display the results of different relational operators and 
+//      logical operators in Java on two Integer values and two Boolean values. Input values to be given within the program
+class third
+{
+	public static void main(String args[])
+	{
+    System.out.println(7<=5);
+	System.out.println(true==true);
+	System.out.println(7==4);
+	System.out.println(true&&false);
+	System.out.println(true||false);	
+}
+}
