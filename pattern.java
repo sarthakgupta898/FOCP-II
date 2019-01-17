c235549d86d14e7161c5a332ec0696fcfdb85bda
sarthak gupta <sarthak.gupta129@gmail.com>
2019-01-17 12:14:44 +0530

    Q7.exercise question control statement & data types

---
+++ pattern.java
@@ -0,0 +1,80 @@
+//      A
+//     ABA
+//    ABCBA
+//   ABCDCBA
+//  ABCDEDCBA
+// ABCDEFEDCBA
+//ABCDEFGFEDCBA
+// ABCDEFEDCBA
+//  ABCDEDCBA
+//   ABCDCBA
+//    ABCBA
+//     ABA
+//      A
+
+import java.util.*;
+
+class pattern
+{
+    public static void main(String args[])
+    {
+        int r, i, k, j,l,n;
+        char let='a';
+        Scanner scan =new Scanner(System.in);
+       do
+       {
+            System.out.print("Enter Number of Rows : ");
+            r = scan.nextInt();
+            
+            for(i=0;i<r;i++)
+            {
+                for(k=r; k>i; k--)
+                {
+                    System.out.print(" ");
+                }
+                let = 'A';
+                for(j=0;j<=i;j++)
+                {
+                    System.out.print(let);
+                    let++;
+                }
+                let--;
+                for(l=0;l<=i;l++)
+                {
+                    let--;
+                    if(let>='A')
+                    {
+                        System.out.print(let);
+                    }    
+                }
+                System.out.println("");
+            }
+            for(i=(r-1);i>0;i--)
+            {
+                for(k=(r); k>=i; k--)
+                {
+                    System.out.print(" ");
+                }
+                let = 'A';
+                for(j=0;j<i;j++)
+                {
+                    System.out.print(let);
+                    let++;
+                }
+                let--;
+                for(l=0;l<=i;l++)
+                {
+                    let--;
+                    if(let>='A')
+                    {
+                        System.out.print(let);
+                    }    
+                }
+                System.out.println("");
+            }
+            System.out.println("Press 1 to try again and 2 to exit");
+            n = scan.nextInt();
+        }
+        while(n==1);
+    }
+}
