9ad0ec5dd9fe3f49d78f80d7a788de9f695671d0
sarthak gupta <sarthak.gupta129@gmail.com>
2019-01-17 12:05:35 +0530

    Q2.exercise question data types & control statements

---
+++ data types.java
@@ -0,0 +1,16 @@
+//2.	Repeat exercise 4 using all primitive data types of Java. Analyse the result obtained for different data types 
+class second
+{
+	public static void  main(String[] args)
+	{
+	char ch='A';
+	ch++;
+	System.out.println(ch);
+    char ch1='B';
+    System.out.println(ch+ch1);
+	String str1,str2;
+	str1="New Delhi";
+	str2="Gurgaon";
+	System.out.println(str1+str2);
+	}
+}
